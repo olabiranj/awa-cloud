fcd953b47b5d4c2e030ae16ad60ee49844c6a28e
Olabiran Joshua <olabiranj@gmail.com>
2020-03-12 08:18:00 +0100

    build v

---
+++ build/precache-manifest.1f58b5781a31b754fde6275746327543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ba8f1b22997a2fcc7bd7f51891227f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48246eac1cb7f26f2c28",
+    "url": "/static/css/main.925710a4.chunk.css"
+  },
+  {
+    "revision": "b553a82a5cdeff133349",
+    "url": "/static/js/2.6b9a9e47.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6b9a9e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48246eac1cb7f26f2c28",
+    "url": "/static/js/main.47be7a03.chunk.js"
+  },
+  {
+    "revision": "a24b77609bc8226cd389",
+    "url": "/static/js/runtime-main.5bb40334.js"
+  }
+]);
