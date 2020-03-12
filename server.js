414f84fb12165ceb00cb23043fea9a872f398943
Olabiran Joshua <olabiranj@gmail.com>
2020-03-12 23:59:48 +0100

    build v

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8000;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
-    return res.send('pong');
+    return res.send('ping');
 });
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    console.log('connected')
+    res.sendFile(path.join(__dirname, 'build', 'index.html')); //serving build folder
 });
 app.listen(port);
