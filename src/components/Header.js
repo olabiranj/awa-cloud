7a52baa1665837842f12d1116d68dbac17c0874c
Olabiran Joshua <olabiranj@gmail.com>
2020-03-24 10:45:40 +0100

    modal bug removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const Header = () => (
-    <ul className="nav justify-content-center bg-dark py-1">
+    <ul className="nav py-1 ml-4">
         <h1 className='text-light header-text'>Awa Cloud</h1>
     </ul>
 )
--- src/views/Index.js
+++ src/views/Index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Weather from '../components/Weather';
 import Search from '../components/Search';
 import Modal from '../components/Modal';
+import Header from '../components/Header';
 
 const Index = () => {
     return (
         <>
+            <Header />
             <Modal />
             <Search/>
             <Weather/>
