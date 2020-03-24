1e730d0862db617e7a8b3917eaf0a89f3b2f4ce7
Olabiran Joshua <olabiranj@gmail.com>
2020-03-24 10:59:41 +0100

    modal bug removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 
 
-const Header = () => {
-
-    return(
-        <ul className="nav pt-2 pl-3">
-            <h1 className='text-light header-text'>Awa Cloud</h1>
-        </ul>
-    )
-}
+const Header = () => (
+    <ul className="nav justify-content-center pt-2">
+        <h2 className='text-light header-text'>Awa Cloud</h2>
+    </ul>
+)
 
 export default Header;
