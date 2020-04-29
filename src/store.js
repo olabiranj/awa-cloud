371db12deba0e4c6cdcf10740bc2fa76ddd0e393
Olabiran Joshua <olabiranj@gmail.com>
2020-04-29 15:42:08 +0100

    redux dev tools removed in production

--- src/store.js
+++ src/store.js
@@ -1,16 +1,18 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducer";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleWare = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(...middleWare))
-);
+
+const devTools =
+    process.env.NODE_ENV === "production"
+        ? applyMiddleware(...middleWare)
+        : composeEnhancers(applyMiddleware(...middleWare));
+
+const store = createStore(rootReducer, initialState, devTools);
 
 export default store;
